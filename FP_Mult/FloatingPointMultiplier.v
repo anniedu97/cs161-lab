929d1457a44dd46a726ea6b24e512e0c5129a6ff
danay001 <danay001@ucr.edu>
2019-03-15 13:59:27 -0700

    Initial commit

---
+++ FP_Mult/FloatingPointMultiplier.v
@@ -0,0 +1,104 @@
+`timescale 1ns / 1ps
+
+module floatingPointMultiplier(
+    input clk,
+    input rst,
+    input enable,
+    output reg valid,
+    input[31:0] A,
+    input[31:0] B,
+    output reg[31:0] result
+);
+
+// Implement your solution here
+
+reg [31:0] yourresult;
+
+reg [7:0] expA;
+reg [7:0] expB;
+reg [7:0] expR;
+reg sign;
+reg [23:0] A_expand;
+reg [23:0] B_expand;
+reg [47:0] Mult;
+reg [22:0] Normie;
+
+integer shift_amt;
+
+
+always @(posedge clk) begin
+	expA = A[30:23];
+	expB = B[30:23];
+	
+	//Calculate Exponent
+	expR = $signed(expA) + $signed(expB);
+	expR = expR - 127;
+	
+	//Sign
+	sign = A[31] ^ B[31];
+	
+	//EXPAND Dong
+	A_expand = A[22:0];
+	B_expand = B[22:0];
+	
+	if(expA == 0) begin
+		A_expand[23] = 1'b0;
+	end
+	
+	else begin
+		A_expand[23] = 1'b1;
+	end
+	
+	if(expB == 0) begin
+		B_expand[23] = 1'b0;
+	end
+	
+	else begin
+		B_expand[23] = 1'b1;
+	end
+	
+	Mult = A_expand * B_expand;
+	
+	shift_amt = 1;
+	
+	while(Mult[47] != 1 && shift_amt < 47) begin
+		Mult = Mult << 1;
+		shift_amt = shift_amt + 1;
+	
+	end
+	
+	Normie = Mult[47:24];
+	
+	expR = expR + shift_amt;
+	
+	
+	
+	yourresult = {sign, expR, Normie};
+	
+	if(A == 0 || B == 0) begin
+		yourresult = 0;
+	end
+	
+	else if(A == 32'h7fffffff || B == 32'h7fffffff) begin
+		yourresult = 32'h7fffffff;
+	end
+	
+
+
+end
+
+
+
+// Sequential section
+always @(posedge clk) begin
+    if(rst) begin
+        result <= 0;
+        valid <= 0;
+    end
+    else begin
+        result <= yourresult;
+        valid <= enable;
+    end
+end
+
+endmodule
---
+++ FP_Mult/floatingPointMultiplier_tb.v
@@ -0,0 +1,116 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   12:38:56 03/15/2019
+// Design Name:   floatingPointMultiplier
+// Module Name:   C:/Users/Anaya/Documents/CS161L/FP_Mult/floatingPointMultiplier_tb.v
+// Project Name:  FP_Mult
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: floatingPointMultiplier
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module floatingPointMultiplier_tb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg enable;
+	reg [31:0] A;
+	reg [31:0] B;
+
+	// Outputs
+	wire valid;
+	wire [31:0] result;
+
+	// Instantiate the Unit Under Test (UUT)
+	floatingPointMultiplier uut (
+		.clk(clk), 
+		.rst(rst), 
+		.enable(enable), 
+		.valid(valid), 
+		.A(A), 
+		.B(B), 
+		.result(result)
+	);
+
+		initial begin
+		// Initialize Inputs
+		clk = 1;
+		rst = 1;
+		enable = 0;
+		A = 0;
+		B = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		
+		clk = 0;
+		rst = 0;
+		enable = 0;
+		A = 0;
+		B = 0;
+		
+		#100
+		
+		B = 32'h40a80000; //5.25	
+		A = 32'h40700000; //3.75   0x419D8000
+		
+		#100;
+		
+		A = 32'h40a80000; //5.25	
+		B = 32'h40700000; //3.75
+		
+		#100
+		
+		
+		A = 32'h42c88000; //100.25
+		B = 32'h40b80000;	//5.75	0x44101c00
+		
+		#100
+		
+		A = 32'hc0f00000; //-7.5
+		B = 32'hc15c0000; //-13.75  0x42ce4000
+		
+		#100
+		
+		A = 32'h41da0000;//27.25
+		B = 32'hc0e80000;//-7.25
+		
+		#100
+		
+		A = 32'h40a80000; //5.22
+		B = 0;
+		
+		#100
+		
+		A = 32'H7fffffff; //NaN
+		B = 32'h42c88000; //100.25
+		
+		
+		
+		
+        
+		// Add stimulus here
+		
+
+	end
+	
+	always 
+			#20 clk = ~clk;
+      
+      
+endmodule
+
